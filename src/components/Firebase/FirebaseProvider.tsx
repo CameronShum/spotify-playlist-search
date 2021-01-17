92e52fba829a48935e65ba3f03f8794639138a24
cameronshum <cameronshum@hotmail.com>
2021-01-16 20:31:45 -0800

    Fix firebase error

--- src/components/Firebase/FirebaseProvider.tsx
+++ src/components/Firebase/FirebaseProvider.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useReducer, useState } from 'react';
-import firebase from 'firebase/app';
+import firebase from 'firebase';
 import 'firebase/database';
 import dotenv from 'dotenv';
 import Nominations from 'types/Nominations.types';
